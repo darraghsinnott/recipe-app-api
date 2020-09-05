15055e445c48d308126617df11043058e9f21314
darraghsinnott <darraghsinnott@mac.com>
2020-09-05 16:04:21 +0000

    Re-factored tests and ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -72,7 +72,7 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
-    
+
     def test_create_ingredient_sucessful(self):
         """
         Test create a new ingredient
